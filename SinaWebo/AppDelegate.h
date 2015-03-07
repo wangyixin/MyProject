e719bde76c70a50a731eea15cf6327d80f5be3c0
wyx <wfwandwyx@163.com>
2015-03-07 10:42:06 +0800

    Initial Commit

---
+++ SinaWebo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  SinaWebo
+//
+//  Created by 亦心 on 15/3/7.
+//  Copyright (c) 2015年 亦心. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
